9602cb214506e27e09e54711557c6db0e4161d56
AmanSong <amansong70@gmail.com>
2023-10-11 21:27:48 +0100

    finished majority of problem 1

--- RichWeb_Week4_lab2/Problem1/phone.js
+++ RichWeb_Week4_lab2/Problem1/phone.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function(e) {
 
     // for adding contacts
     document.getElementById("add-contact").addEventListener("click", function(e) {
@@ -55,3 +55,101 @@ function enforceMaxLength(input, maxLength) {
         input.value = input.value.slice(0, maxLength);
     }
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    const table = document.querySelector("#table");
+    const searchInput = document.querySelector("#search");
+    const noInfoMessage = document.querySelector("#noInfoMessage");
+
+    // Sample data for testing
+    const data = [
+        { name: "Aman Song", mobile: 18449291, email: "song@example.com" },
+        { name: "John Doe", mobile: 44234567, email: "john@example.com" },
+        { name: "Jane Smith", mobile: 55598765, email: "jane@example.com" },
+        { name: "Bob Johnson", mobile: 77543780, email: "bob@example.com" },
+        { name: "Alice Brown", mobile: 13577788, email: "alice@example.com" },
+    ];
+
+    // Create rows with sample data
+    data.forEach(item => {
+        const newRow = table.insertRow();
+        newRow.classList.add("row");
+        const nameCell = newRow.insertCell();
+        nameCell.classList.add("table-item");
+        nameCell.textContent = item.name;
+        const mobileCell = newRow.insertCell();
+        mobileCell.classList.add("table-item", "mobile-cell");
+        mobileCell.textContent = item.mobile; // Numeric value
+        const emailCell = newRow.insertCell();
+        emailCell.classList.add("table-item");
+        emailCell.textContent = item.email;
+    });
+
+    // Get all rows in the table except the first row
+    const rows = table.querySelectorAll(".row:not(.header)");
+
+    // Add an input event listener to the search input
+    searchInput.addEventListener("input", function() {
+        const searchText = searchInput.value.trim();
+        let found = false;
+
+    // Loop through each row and hide/show based on the search text
+    rows.forEach(row => {
+    const mobileCell = row.querySelector(".mobile-cell");
+    if (mobileCell) {
+        const mobileValue = parseFloat(mobileCell.textContent); 
+        if (!isNaN(mobileValue) && mobileValue.toString().includes(searchText)) {
+        row.style.display = "";
+        found = true;
+        } else {
+        row.style.display = "none";
+        }
+    }
+    });
+
+        // Show/hide the "No information found" message
+        if (found) {
+        noInfoMessage.style.display = "none";
+        } else {
+        noInfoMessage.style.display = "";
+        }
+    });
+});
+  
+document.addEventListener("DOMContentLoaded", function() {
+    const nameHeader = document.querySelector("#name-header");
+    const table = document.querySelector("#table");
+    const rows = Array.from(table.querySelectorAll(".row:not(:first-child)"));
+
+    let sort = true
+  
+    nameHeader.addEventListener("click", function() {
+        if(sort == true) {
+            rows.sort((a, b) => {
+                const nameA = a.querySelector(".table-item").textContent.toLowerCase();
+                const nameB = b.querySelector(".table-item").textContent.toLowerCase();
+                return nameA.localeCompare(nameB);
+            });
+    
+            // Clear the table and reinsert the sorted rows
+            rows.forEach(row => table.appendChild(row));
+
+            sort = false
+        }
+        else {
+            rows.sort((a, b) => {
+                const nameA = a.querySelector(".table-item").textContent.toLowerCase();
+                const nameB = b.querySelector(".table-item").textContent.toLowerCase();
+                return nameB.localeCompare(nameA);
+            });
+    
+            // Clear the table and reinsert the sorted rows
+            rows.forEach(row => table.appendChild(row));
+
+            sort = true
+        }
+
+    });
+});
+  
+  
