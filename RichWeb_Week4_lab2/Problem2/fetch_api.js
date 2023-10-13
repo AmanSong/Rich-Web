436437b07da4313add0270d1aab2d6d10a2c7906
Aman Song <amansong70@gmail.com>
2023-10-13 20:21:01 +0100

    doing problem 2

---
+++ RichWeb_Week4_lab2/Problem2/fetch_api.js
@@ -0,0 +1,27 @@
+fetch('https://jsonplaceholder.typicode.com/posts')
+  .then(response => response.json())
+  .then(posts => {
+
+    // map all titles to new array
+    const titles = posts.map(posts => posts.title);
+
+    // use filter to find titles with more than 6 words
+    const result = titles.filter(title => title.split(' ').length > 6);
+
+    // print out result
+    console.log(result);
+});
+
+fetch('https://jsonplaceholder.typicode.com/posts')
+  .then(response => response.json())
+  .then(posts => {
+
+    const words = posts.flatMap((posts) => posts.body.split(' '))
+
+    const frequency = words.reduce((map, word) => {
+        map[word] = (map[word] || 0) + 1;
+        return map;
+      }, {});
+
+    console.log(frequency)
+});
