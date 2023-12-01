2545980c42d14bc42e027529c0b29bfc61c78d68
AmanSong <amansong70@gmail.com>
2023-12-01 20:36:41 +0000

    starting with components usage

--- RichWeb_Lab4/problem_1/src/App.js
+++ RichWeb_Lab4/problem_1/src/App.js
@@ -3,60 +3,13 @@ import React, { useState } from 'react';
 
 function App() {
 
-  const [inputValue, setInputValue] = useState('');
-  const [selectedValue, setSelectedValue] = useState('');
-
-  const handleInputChange = (e) => {
-    setInputValue(e.target.value);
-  };
-
-  const handleDropdownChange = (e) => {
-    setSelectedValue(e.target.value);
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log('Input submitted:', inputValue, selectedValue);
-    setInputValue('');
-  };
-
   return (
 
     <div className="App">
       <header className="App-header">
         <div className="main">
 
-          <div className="note">
-            <form onSubmit={handleSubmit}>
-              <textarea
-                type="text"
-                value={inputValue}
-                onChange={handleInputChange}
-                placeholder="Your note..."
-              />
-            </form>
-          </div>
 
-          <div className="color">
-            <form onSubmit={handleSubmit}>
-              <select
-                value={selectedValue}
-                onChange={handleDropdownChange}
-              >
-                <option value="red">Red</option>
-                <option value="blue">Blue</option>
-                <option value="green">Green</option>
-              </select>
-            </form>
-          </div>
-
-          <div className='button-container'>
-            <button className='button' onClick={handleSubmit}>Create Note</button>
-          </div>
-
-          <div className="notes-container">
-
-          </div>
         </div>
       </header>
     </div>
